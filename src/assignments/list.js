4bf92f32862be966ebd5e713dedf8e7b203be651
Rawan-i <ralbanna806@gmail.com>
2025-12-06 10:32:07 +0300

    Update list.js

--- src/assignments/list.js
+++ src/assignments/list.js
@@ -13,6 +13,7 @@
 
 // --- Element Selections ---
 // TODO: Select the section for the assignment list ('#assignment-list-section').
+const listSection = document.getElementById("assignment-list-section");
 
 // --- Functions ---
 
@@ -25,6 +26,27 @@
  */
 function createAssignmentArticle(assignment) {
   // ... your implementation here ...
+  const article = document.createElement("article");
+
+  const h2 = document.createElement("h2");
+  h2.textContent = assignment.title;
+  article.appendChild(h2);
+
+  const dueP = document.createElement("p");
+  dueP.textContent = "Due: " + assignment.dueDate;
+  article.appendChild(dueP);
+
+  const descP = document.createElement("p");
+  descP.textContent = assignment.description;
+  article.appendChild(descP);
+
+  const link = document.createElement("a");
+  link.href = `details.html?id=${assignment.id}`;
+  link.textContent = "View Details & Discussion";
+  article.appendChild(link);
+
+  return article;
+
 }
 
 /**
@@ -40,6 +62,17 @@ function createAssignmentArticle(assignment) {
  */
 async function loadAssignments() {
   // ... your implementation here ...
+  const response = await fetch("api/assignments.json");
+
+  const assignments = await response.json();
+
+  listSection.innerHTML = "";
+
+  assignments.forEach(assignment => {
+      const article = createAssignmentArticle(assignment);
+      listSection.appendChild(article);
+    });
+
 }
 
 // --- Initial Page Load ---
